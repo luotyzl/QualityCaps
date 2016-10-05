3cf6cf141721239a41cc44f3f6f379a9c633141a
Raymon <luotyzl@gmail.com>
2016-10-05 16:20:11 +0800

    add map

--- QualityCaps/Controllers/HomeController.cs
+++ QualityCaps/Controllers/HomeController.cs
@@ -13,18 +13,8 @@ namespace QualityCaps.Controllers
             return View();
         }
 
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
